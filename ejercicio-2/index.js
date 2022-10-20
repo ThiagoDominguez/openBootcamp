e66e31f9e08c851370bab4806740090081f7fc07
ThiagoDominguez <credgeyt@gmail.com>
2022-10-20 14:45:07 -0300

    ejercicio-2

---
+++ ejercicio-2/index.js
@@ -0,0 +1,14 @@
+let name = "Thiago Dominguez";
+let edad = 21;
+let desarrollador = true;
+
+let fecNac = new Date(2001, 7, 31);
+
+// No tengo un libro favorito asi que voy a simular uno :)
+
+const libro = {
+  titulo: "El Libro null",
+  autor: "Yo",
+  fecha: new Date(),
+  url: "https://github.com/ThiagoDominguez",
+};
