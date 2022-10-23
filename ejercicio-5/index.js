119713caf1a45b280efb9fce610645462d465292
ThiagoDominguez <credgeyt@gmail.com>
2022-10-23 15:47:29 -0300

    ejercicio-5

---
+++ ejercicio-5/index.js
@@ -0,0 +1,13 @@
+let altura_cm = 171;
+
+let altura_mts = 1.71;
+
+let peso = 55.5;
+
+let altura_round = Math.round(altura_mts);
+
+let peso_floor = Math.floor(peso);
+
+let max = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+console.log(max);
