4c705f6791f1541bdeef023ee8279152761aac4c
ThiagoDominguez <credgeyt@gmail.com>
2022-10-26 17:26:59 -0300

    ejercicio-7

---
+++ ejercicio-7/conjuntos.js
@@ -0,0 +1,17 @@
+const setFamily = new Set([
+  "Claudio",
+  "Giuliana",
+  "Daiana",
+  "Sandra",
+  "Thiago",
+]);
+
+console.log(setFamily);
+
+const dupSetFamily = setFamily.add("Thiago");
+
+console.log(dupSetFamily);
+
+const jsSetFamily = setFamily.add("JavaScript");
+
+console.log(jsSetFamily);
---
+++ ejercicio-7/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+// Trabajando con fechas
+
+const fecha = new Date();
+
+const fecha2 = new Date(2020, 03, 23);
+
+console.log(fecha2);
+
+const fecha3 = new Date(1000000000000);
+
+console.log(fecha3);
