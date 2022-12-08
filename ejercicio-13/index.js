4fea8fdaeb6a8305ae98f49f2558c9bae1776862
ThiagoDominguez <credgeyt@gmail.com>
2022-12-08 18:40:16 -0300

    ejercicio-13

---
+++ ejercicio-13/index.js
@@ -0,0 +1,15 @@
+let name = "Thiago ";
+
+let lastName = "Dominguez ";
+
+let estudiante = `${name} ${lastName}`;
+
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+
+let lettersEstudiante = estudiante.length;
+
+let firstLetterName = name[0];
+let lastLetterLastName = lastName[lastName.length - 1];
+let estudianteNoSpace = name.trim() + lastName.trim();
+let IsName = estudiante.includes(name);
