be5a1f626f543ae8a06f3741c05420153abc00c3
ThiagoDominguez <credgeyt@gmail.com>
2022-12-12 17:01:08 -0300

    ejercicio-15

---
+++ ejercicio-15/index.js
@@ -0,0 +1,20 @@
+let nombre = "Thiago";
+
+let apellido = "Dominguez";
+
+const persona = {
+  nombre,
+  apellido,
+};
+
+// sessionStorage.setItem("persona", JSON.stringify(persona));
+
+// localStorage.setItem("persona", JSON.stringify(persona));
+
+// document.cookie = "persona=" + JSON.stringify(persona);
+
+// document.cookie = "nombre-caducidad=nombre;expires=" + new Date().toUTCString();
+
+// document.cookie =
+//   "nombre-caducidad=nombre;expires=" +
+//   new Date(2022, 11, 12, 18, 46).toUTCString();
